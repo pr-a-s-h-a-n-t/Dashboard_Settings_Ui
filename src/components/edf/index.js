8ca74e5db521cd9f6a2e37a6e2eb1e534114f434
Prashant Mishra <prashantmishra111@outlook.com>
2023-01-28 08:55:27 +0530

    upload pdf feature added!!

--- src/components/edf/index.js
+++ src/components/edf/index.js
@@ -1,13 +1,54 @@
-import React from "react";
+import React,{ useRef, useState } from "react";
 import "./edf.css";
+import edfLogo from "../../assets/edfupload/edfLogo.png";
 
 function Upload() {
-  return (
+  let inputRef = React.createRef();
+  const [uploadLoading, setUploadLoading] = useState(0);
+  const [edf, setEdf] = useState(null);
+
+
+
+
+  const uploadLogo = (e) => {
+    let file = e.target.files[0];
+    setUploadLoading(1);
+    setEdf(file)
+    // inputRef.current = edf;
+    console.log("edf", edf, "==============>",inputRef.current);
+    //ref(storage,'path to file',file,name)
     
-  <div className="upload-conatainer">
-    upload section
-  </div>
-  )
+    //uploadBytesResumable(storage-Ref,file)
+    // const uploadTask = uploadBytesResumable(storageRef, file);
+
+     
+  };
+
+  return (
+    <div className="upload-edfs-conatainer">
+      <div className="edfs-upload">
+        <div className="upload">
+          <img width=" 100px" height=" 100px" src={edfLogo} alt="edfLogo" />
+
+          <input
+            accept="pdf/*"
+            style={{
+              display: "none",
+            }}
+            ref={inputRef}
+            type={"file"}
+            // value={" "}
+            onChange={(e) => uploadLogo(e)}
+          />
+          <button id="upload-btn" onClick={() => inputRef.current.click()}>
+            Browse Files
+          </button>
+          <h5>or drop files here</h5>
+        </div>
+         
+      </div>
+    </div>
+  );
 }
 
 export default Upload;
