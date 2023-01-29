6e9e3c6f453b7fd836a75e20ef7ea68a1f8beba1
Prashant Mishra <prashantmishra111@outlook.com>
2023-01-29 19:13:34 +0530

    additionalbackupchannel logic implemented!

--- src/components/mapchannels/index.js
+++ src/components/mapchannels/index.js
@@ -5,6 +5,65 @@ import CustomDropDown from "../../components/common/customdropdown/CustomDropDow
 import { primaryChannel, referenceChannel } from "../randomdata/index";
 import { Button } from "@mui/material";
 
+// --------------------------
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+//----------------------------
+
+const dropDownListItem = [
+  {
+    value: "a1",
+    label: "A1",
+  },
+  {
+    value: "b1",
+    label: "B1",
+  },
+  {
+    value: "c1",
+    label: "C1",
+  },
+  {
+    value: "d1",
+    label: "D1",
+  },
+  {
+    value: "e1",
+    label: "E1",
+  },
+  {
+    value: "f1",
+    label: "F1",
+  },
+  {
+    value: "g1",
+    label: "G1",
+  },
+  {
+    value: "h1",
+    label: "H1",
+  },
+  {
+    value: "i1",
+    label: "I1",
+  },
+  {
+    value: "j1",
+    label: "J1",
+  },
+  {
+    value: "k1",
+    label: "K1",
+  },
+  {
+    value: "l1",
+    label: "L1",
+  },
+];
+
 const columns = [
   {
     title: "Channel",
@@ -36,9 +95,18 @@ function MapChannels({ stepperValue, setStepperValue }) {
     optional1,
     optional2,
   });
+  let ss;
+  //  ss = document.write(<CustomDropDown /> )
   const [isChecked, setIsChecked] = useState(false);
   const [backupChannel, setBackupChannel] = useState(false);
-
+  const [addBackUpChannel, setAddBackUpChannel] = useState({
+    ss: "",
+  });
+  let temp = [ ];
+  const [addBackUpChannelSelect, setAddBackUpChannelSelect] = useState([
+    ...temp,
+  ]);
+  console.log(addBackUpChannelSelect, "=======>");
   // useEffect(()=>{
   //   setStepperValue(1);
   // }, [])
@@ -65,73 +133,155 @@ function MapChannels({ stepperValue, setStepperValue }) {
           <div className="table-row-container">
             {ChannelsData.channels.map((column, index) => {
               return (
-                <div className="table-row" key={index}>
-                  <div>{column}</div>
-                  <div className="channels-select-container">
-                    {/* <label className=" mapChannel-label">Edit Channel</label> */}
-                    <div>
-                      <CustomDropDown
-                        required={true}
-                        dropDownList={primaryChannel}
-                        val={values.primaryChannel}
-                        onChange={(data) =>
-                          setValues({ ...values, primaryChannel: data })
-                        }
-                      />
-                    </div>
+                <div>
+                  <div className="table-row" key={index}>
+                    <div>{column}</div>
+                    <div className="channels-select-container">
+                      {/* <label className=" mapChannel-label">Edit Channel</label> */}
+                      <div>
+                        <CustomDropDown
+                          required={true}
+                          dropDownList={primaryChannel}
+                          val={values.primaryChannel}
+                          onChange={(data) =>
+                            setValues({ ...values, primaryChannel: data })
+                          }
+                        />
+                      </div>
 
-                    {/* <label className=" refChannel-label">Edit Channel</label> */}
-                    <div>
-                      <CustomDropDown
-                        required={true}
-                        dropDownList={referenceChannel}
-                        val={values.refChannel}
-                        onChange={(data) =>
-                          setValues({ ...values, refChannel: data })
-                        }
-                      />
-                    </div>
+                      {/* <label className=" refChannel-label">Edit Channel</label> */}
+                      <div>
+                        <CustomDropDown
+                          required={true}
+                          dropDownList={referenceChannel}
+                          val={values.refChannel}
+                          onChange={(data) =>
+                            setValues({ ...values, refChannel: data })
+                          }
+                        />
+                      </div>
 
-                    <div>
-                      <Button
-                        variant="outlined"
-                        onClick={() => setBackupChannel(!backupChannel)}
-                        sx={{
-                          width: "fit-content",
-                          height: "21px",
-                          fontFamily: "DM Sans",
-                          fontStyle: "normal",
-                          fontWeight: " 500",
-                          fontSize: "16px",
-                          lineHeight: "21px",
-                          // display: "flex",
-                          alignItems: " center",
-
-                          color: " #2F7EC7",
-                        }}
-                      >
-                        Hide backup channels (0)
-                      </Button>
+                      <div>
+                        <Button
+                          variant="outlined"
+                          onClick={() => setBackupChannel(!backupChannel)}
+                          sx={{
+                            width: "fit-content",
+                            height: "21px",
+                            fontFamily: "DM Sans",
+                            fontStyle: "normal",
+                            fontWeight: " 500",
+                            fontSize: "16px",
+                            lineHeight: "21px",
+                            // display: "flex",
+                            alignItems: " center",
+                            color: " #2F7EC7",
+                          }}
+                        >
+                          Hide backup channels (0)
+                        </Button>
+                      </div>
                     </div>
+                  </div>
+                  <div className="additional-backupChannel-container">
+                    {backupChannel ? (
+                      <div style={{}}>
+                        <div  >
+                         { addBackUpChannelSelect.length != 0 && addBackUpChannelSelect.length > 0 ?
+                          addBackUpChannelSelect.map((item, i) => {
+                           return  <div className="backup-channels" key={i}>
+                            <div>
+                              <FormControl sx={{ width: "100%" }}>
+                                <Select
+                                  fullWidth
+                                  size="small"
+                                  label="Select"
+                                  sx={{
+                                    width: "274px",
+                                    height: "40px",
+                                    border: "1px solid black",
+                                  }}
+                                  // disabled={disabled}
+                                  // required={required}
+                                  id="demo-simple-select"
+                                  // value={val}
+                                  // onChange={handleChange}
+                                >
+                                  {dropDownListItem.map((item, id) => (
+                                    <MenuItem key={id} value={item.value}>
+                                      {item.label}
+                                    </MenuItem>
+                                  ))}
+                                </Select>
+                              </FormControl>
+                            </div>
+                            <div>
+                              <FormControl sx={{ width: "100%" }}>
+                                <Select
+                                  fullWidth
+                                  size="small"
+                                  label="Select"
+                                  sx={{
+                                    width: "274px",
+                                    height: "40px",
+                                    border: "1px solid black",
+                                  }}
+                                  // disabled={disabled}
+                                  // required={required}
+                                  id="demo-simple-select"
+                                  // value={val}
+                                  // onChange={handleChange}
+                                >
+                                  {dropDownListItem.map((item, id) => (
+                                    <MenuItem key={id} value={item.value}>
+                                      {item.label}
+                                    </MenuItem>
+                                  ))}
+                                </Select>
+                              </FormControl>
+                            </div>
+                            <div>
+                            <Button>Delete</Button>
+                          </div>
+                          </div>
+                          })
+                            
+                          : " "
+                          }
+
+                           
+                        </div>
+                        <div className="backup-channels-btn">
+                          <button
+                            variant="outlined"
+                            onClick={() =>
+                              setAddBackUpChannelSelect((prev) => [...prev, "1"])
+                            }
+                            // sx={{
+                            //   width: "fit-content",
+                            //   height: "21px",
+                            //   fontFamily: "DM Sans",
+                            //   fontStyle: "normal",
+                            //   fontWeight: " 500",
+                            //   fontSize: "16px",
+                            //   lineHeight: "21px",
+                            //   // display: "flex",
+                            //   alignItems: " center",
+                            //   color: " #2F7EC7",
+                            // }}
+                          >
+                            +Add backup channels
+                          </button>
+                        </div>
+                      </div>
+                    ) : (
+                      ""
+                    )}
                   </div>
                 </div>
               );
             })}
-            <div>
-              {backupChannel ? (
-                <div
-                  style={{
-                    width: "100%",
-                    height: "49px",
-                    display: "block",
 
-                    border: "1px solid green",
-                  }}
-                ></div>
-              ) : (
-                ""
-              )}
-            </div>
             <div className="additional-setting-contianer">
               <div className="additional-setting-contianer-item">
                 Additional Settings
