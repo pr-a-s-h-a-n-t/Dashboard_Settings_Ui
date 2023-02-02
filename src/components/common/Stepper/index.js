48e5d563690812e878aa3c4c9e6d0aa24103d2e0
Prashant Mishra <prashantmishra111@outlook.com>
2023-02-02 12:43:40 +0530

    stepper css completed!!!

--- src/components/common/Stepper/index.js
+++ src/components/common/Stepper/index.js
@@ -1,90 +1,5 @@
-// import React from "react";
-// import "./Stepper.css";
-
-// export default function Stepper() {
-//   return (
-// <div className="stepper-container">
-// {
-/* <div className="stepper-elements">
-        <span>1</span>
-      </div>
-
-      <div className="stepper-elements">
-        <span>2</span>
-      </div>
-
-      <div className="stepper-elements">
-        <span>3</span>
-      </div> */
-// }
-//     </div>
-//   );
-// }
-
-// import * as React from "react";
-// import Box from "@mui/material/Box";
-// import Stepper from "@mui/material/Stepper";
-// import Step from "@mui/material/Step";
-// import StepLabel from "@mui/material/StepLabel";
-// import "./Stepper.css";
-// import { colors, createTheme, ThemeProvider } from "@mui/material";
-
-// const steps = ["1", "2", "3"];
-// export default function StepperComponent() {
-//   const theme = createTheme({
-//      typography: { fontSize: 30,  },
-
-//     });
-//   return (
-//     <Box
-//       sx={{
-//         width: "100%",
-//         border: "1px solid red",
-//         padding: "20px",
-//         // display: "flex",
-//         // justifyContent: "space-around",
-//         // gap: "40px",
-//         alignItems: "center",
-//       }}
-//     >
-//       {" "}
-//       <ThemeProvider theme={theme}>
-//         <Stepper
-//           // sx={{
-//           //   width: "100%",
-//           //   border: "10px solid white",
-//           // }}
-//           activeStep={}
-//           alternativeLabel
-//         >
-//           {steps.map((label) => (
-//             <Step
-//               // sx={{
-//               //   // width: "100%",
-//               //   border: "10px solid orange",
-//               //   borderRadius: "50%",
-//               // }}
-//               key={label}
-//             >
-//               <StepLabel
-//                 sx={
-//                   {
-//                     //  width: '100%',
-//                     //  border: '10px solid green',
-//                   }
-//                 }
-//               >
-//                 {}
-//               </StepLabel>
-//             </Step>
-//           ))}
-//         </Stepper>
-//       </ThemeProvider>
-//     </Box>
-//   );
-// }
-
 import * as React from "react";
+import "./Stepper.css";
 import PropTypes from "prop-types";
 import { styled } from "@mui/material/styles";
 import Stack from "@mui/material/Stack";
@@ -95,100 +10,24 @@ import Check from "@mui/icons-material/Check";
 import SettingsIcon from "@mui/icons-material/Settings";
 import GroupAddIcon from "@mui/icons-material/GroupAdd";
 import VideoLabelIcon from "@mui/icons-material/VideoLabel";
-
-import "./Stepper.css";
-
 import StepConnector, {
   stepConnectorClasses,
 } from "@mui/material/StepConnector";
 
-const QontoConnector = styled(StepConnector)(({ theme }) => ({
-  [`&.${stepConnectorClasses.alternativeLabel}`]: {
-    top: 10,
-    left: "calc(-50% + 16px)",
-    right: "calc(50% + 16px)",
-  },
-  [`&.${stepConnectorClasses.active}`]: {
-    [`& .${stepConnectorClasses.line}`]: {
-      borderColor: "#784af4",
-    },
-  },
-  [`&.${stepConnectorClasses.completed}`]: {
-    [`& .${stepConnectorClasses.line}`]: {
-      borderColor: "#784af4",
-    },
-  },
-  [`& .${stepConnectorClasses.line}`]: {
-    borderColor:
-      theme.palette.mode === "dark" ? theme.palette.grey[800] : "#eaeaf0",
-    borderTopWidth: 3,
-    borderRadius: 1,
-  },
-}));
-
-const QontoStepIconRoot = styled("div")(({ theme, ownerState }) => ({
-  color: theme.palette.mode === "dark" ? theme.palette.grey[700] : "#eaeaf0",
-  display: "flex",
-  height: 22,
-  alignItems: "center",
-  ...(ownerState.active && {
-    color: "#784af4",
-  }),
-  "& .QontoStepIcon-completedIcon": {
-    color: "#784af4",
-    zIndex: 1,
-    fontSize: 18,
-  },
-  "& .QontoStepIcon-circle": {
-    width: 8,
-    height: 8,
-    borderRadius: "50%",
-    backgroundColor: "currentColor",
-  },
-}));
-
-function QontoStepIcon(props) {
-  const { active, completed, className } = props;
-
-  return (
-    <QontoStepIconRoot ownerState={{ active }} className={className}>
-      {completed ? (
-        <Check className="QontoStepIcon-completedIcon" />
-      ) : (
-        <div className="QontoStepIcon-circle" />
-      )}
-    </QontoStepIconRoot>
-  );
-}
-
-QontoStepIcon.propTypes = {
-  /**
-   * Whether this step is active.
-   * @default false
-   */
-  active: PropTypes.bool,
-  className: PropTypes.string,
-  /**
-   * Mark the step as completed. Is passed to child components.
-   * @default false
-   */
-  completed: PropTypes.bool,
-};
-
 const ColorlibConnector = styled(StepConnector)(({ theme }) => ({
   [`&.${stepConnectorClasses.alternativeLabel}`]: {
     top: 22,
+    // background: "green"
   },
   [`&.${stepConnectorClasses.active}`]: {
     [`& .${stepConnectorClasses.line}`]: {
-      backgroundImage:
-        "linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)",
+      background: "#10A44B",
+      // background: "green"
     },
   },
   [`&.${stepConnectorClasses.completed}`]: {
     [`& .${stepConnectorClasses.line}`]: {
-      backgroundImage:
-        "linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)",
+      background: "#10A44B",
     },
   },
   [`& .${stepConnectorClasses.line}`]: {
@@ -204,21 +43,32 @@ const ColorlibStepIconRoot = styled("div")(({ theme, ownerState }) => ({
   backgroundColor:
     theme.palette.mode === "dark" ? theme.palette.grey[700] : "#ccc",
   zIndex: 1,
-  color: "#fff",
-  width: 50,
-  height: 50,
+  width: "50px",
+  height: " 50px",
+  background: "#fff",
+  color: "#C4C4C4",
+  border: "1px solid #C4C4C4",
   display: "flex",
   borderRadius: "50%",
   justifyContent: "center",
   alignItems: "center",
   ...(ownerState.active && {
-    backgroundImage:
-      "linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)",
-    boxShadow: "0 4px 10px 0 rgba(0,0,0,.25)",
+    width: "50px",
+    height: " 50px",
+    background: "#fff",
+    color: "#10A44B",
+    border: "1px solid #10A44B",
+    borderRadius: "50%",
+    zIndex: 1,
+    fontSize: 18,
   }),
   ...(ownerState.completed && {
-    backgroundImage:
-      "linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)",
+    width: "50px",
+    height: " 50px",
+    background: " #10A44B",
+    borderRadius: "50%",
+    zIndex: 1,
+    fontSize: 18,
   }),
 }));
 
@@ -226,6 +76,12 @@ function ColorlibStepIcon(props) {
   const { active, completed, className } = props;
 
   const icons = {
+    1: <SettingsIcon />,
+    2: <GroupAddIcon />,
+    3: <VideoLabelIcon />,
+  };
+
+  const count = {
     1: "1",
     2: "2",
     3: "3",
@@ -236,26 +92,19 @@ function ColorlibStepIcon(props) {
       ownerState={{ completed, active }}
       className={className}
     >
-      {icons[String(props.icon)]}
+      {completed ? (
+        <Check className="QontoStepIcon-completedIcon" />
+      ) : (
+        count[String(props.icon)]
+      )}
     </ColorlibStepIconRoot>
   );
 }
 
 ColorlibStepIcon.propTypes = {
-  /**
-   * Whether this step is active.
-   * @default false
-   */
   active: PropTypes.bool,
   className: PropTypes.string,
-  /**
-   * Mark the step as completed. Is passed to child components.
-   * @default false
-   */
   completed: PropTypes.bool,
-  /**
-   * The label displayed in the step icon.
-   */
   icon: PropTypes.node,
 };
 
@@ -263,19 +112,7 @@ const steps = ["Upload EDFs", "Map Channels", "Save & Preview"];
 
 export default function StepperComponent({ setStepperValue, stepperValue }) {
   return (
-    <Stack
-      // sx={{ width: "100%" }}
-      sx={{
-        width: "100%",
-        border: "1px solid red",
-        padding: "20px",
-        height: "8.5625rem",
-        // justifyContent: "space-between",
-        // gap: "40px",
-        // alignItems: "center",
-      }}
-      spacing={4}
-    >
+    <Stack sx={{ width: "100%" }} spacing={4}>
       <Stepper
         alternativeLabel
         activeStep={stepperValue}
