a460ced03d3dfe761ff3f6f1394f3a4e5636e8d2
Prashant Mishra <prashantmishra111@outlook.com>
2023-02-03 09:37:43 +0530

    prev btn logic added!!

--- src/components/common/BottomNav/index.js
+++ src/components/common/BottomNav/index.js
@@ -1,51 +1,71 @@
 import { Button, Typography } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./BottomNav.css";
 
-function BottomBav({setStepperValue, stepperValue}) {
-  const [disable, setDisable] = useState("disable-btn");
+function BottomBav({ setStepperValue, stepperValue }) {
+  let btnRef = useRef(false);
+  const [val, setVal] = useState(false);
+  const NavigateToNextPage = () => {
+    if (stepperValue === 0) {
+      console.log("NavigateToNextPage stepper value initial: " + stepperValue);
+      setStepperValue(stepperValue + 1);
+      console.log("NavigateToNextPage stepper value after: " + stepperValue);
+    } else if (stepperValue === 1) {
+      setStepperValue(stepperValue + 1);
+    }
+  };
+  const NavigateToPreviousPage = () => {
+    if (stepperValue === 2) {
+      setStepperValue(stepperValue - 1);
+    } else if (stepperValue === 1) {
+      setStepperValue(stepperValue - 1);
+    }
+  };
+  const stepperValueCheck = (stepperValue) => {
+    if (stepperValue === 0) {
+      // btnRef.current = true;
+      setVal(!val);
+    } else if (stepperValue >= 1) {
+      // btnRef.current = true;
+      setVal(false);
+    }
 
-const NavigateToNextPage = () =>{
-  if(  stepperValue === 0){
-    setStepperValue(stepperValue + 1);
-  
+    console.log("Stepper value check function finish running");
+  };
 
-  }
-  else if( stepperValue === 1){
-    setStepperValue(stepperValue + 1);
-
-
-  }
-}
-const NavigateToPreviousPage = () =>{
-  if(  stepperValue === 2){
-    setStepperValue(stepperValue - 1);
- 
-
-  }
-  else if( stepperValue === 1){
-    setStepperValue(stepperValue - 1);
-
-  }
-}
-
+  useEffect(() => {
+    console.log("NavigateToNextPage stepper value useeffect: " + stepperValue);
+    stepperValueCheck(stepperValue);
+    console.log("btnRef value: ", btnRef.current);
+  }, [stepperValue]);
 
   return (
     <div className="bottomnav-container">
       <div className="bottomnavcontainer-element-start">
         <span>
-          <button className={disable}
-          onClick={() => NavigateToPreviousPage()}
-          >Back</button>
+          <button
+            disabled={val}
+            className={stepperValue >= 1 ? "btn" : "disable-btn"}
+            onClick={() => NavigateToPreviousPage()}
+          >
+            Back
+          </button>
         </span>
         <span id="cancel-montage">
-          <button className="disable-btn">Cancel Montage</button>
+          <button
+            className="btn"
+            //  className="disable-btn"
+          >
+            Cancel Montage
+          </button>
         </span>
       </div>
       <div className="bottomnav-container-element-end">
-        <button className="btn" variant="contained" 
-        sx={{}}
-        onClick={() => NavigateToNextPage()}
+        <button
+          className="btn"
+          variant="contained"
+          sx={{}}
+          onClick={() => NavigateToNextPage()}
         >
           Next
         </button>
